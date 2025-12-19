8f7c0f3a2a9b45a064271ef44895435fa629265c
Tom Bishop <114227637+TomBishop361@users.noreply.github.com>
2025-12-19 14:34:02 +0000

    Position author

---
+++ Source/PortfolioProject/Public/positionComponentAuthor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "componentAuthorBase.h"
+#include "positionComponentAuthor.generated.h"
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class PORTFOLIOPROJECT_API UpositionComponentAuthor : public UcomponentAuthorBase
+{
+	GENERATED_BODY()
+public:
+	
+};
